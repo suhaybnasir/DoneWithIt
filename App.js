09af033198b9f34fbb8cd1fd27c25ca987886cda
Suhayb <suhaybabdinaasir@gmail.com>
2020-09-26 00:37:28 +0300

    Added lists

--- App.js
+++ App.js
@@ -6,7 +6,13 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <ListingsScreen />;
 }
