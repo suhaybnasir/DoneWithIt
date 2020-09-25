d3533a6b7f99913634801ecc47f8cdffba601bff
Suhayb <suhaybabdinaasir@gmail.com>
2020-09-25 13:50:29 +0300

    Added some styles

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
+
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
